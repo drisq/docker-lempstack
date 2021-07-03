dbbdb398d5d13a9257a3c5fcf5e186264282552e
kieljanszen <kiel.legaspi.jose@gmail.com>
2021-07-03 13:39:10 +0800

    reset php index

--- src/index.php
+++ src/index.php
@@ -1,9 +1,3 @@
 <?php
 
-// Test db
-
-$connection = new PDO('mysql:host=mysql;dbname=lempstack;charset=utf8', 'root', 'root');
-$query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'lempstack'");
-$tables = $query->fetchAll(PDO::FETCH_COLUMN);
-
-var_dump($tables);
+phpinfo();
