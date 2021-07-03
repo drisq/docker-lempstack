25792f7cfa953a3750a819cc8cd2c00af78750ec
kieljanszen <kiel.legaspi.jose@gmail.com>
2021-07-03 13:38:43 +0800

    add mysql service plus other db config

--- src/index.php
+++ src/index.php
@@ -1,3 +1,9 @@
 <?php
 
-phpinfo();
+// Test db
+
+$connection = new PDO('mysql:host=mysql;dbname=lempstack;charset=utf8', 'root', 'root');
+$query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'lempstack'");
+$tables = $query->fetchAll(PDO::FETCH_COLUMN);
+
+var_dump($tables);
